16e555e664cfc28f479a92106be9d2628179ce70
YhostinCTC <yollachica@crackthecode.la>
2022-12-30 10:13:26 -0500

    Estos son mis cambios, del profe

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let button = document.getElementById("button_buscar")
+
+let recetas_nombres = [
+    "Suspiro a la limena",
+    "Mini tarta de limon",
+    "Cheesecake de maracuya"
+]
+
+let recetas_claves = [
+    "suspiro_de_limeña.html",
+    "mini trata de limon.html",
+    "cheesecake_de_maracuya"
+]
+
+button.addEventListener("click", function(){
+    let receta = document.getElementById("input_receta")
+
+    for(let i = 0; i < recetas_nombres.length; i++){
+        if(receta.value == recetas_nombres[i]){
+            window.open(recetas_claves[i])
+        }
+    }
+
+})
